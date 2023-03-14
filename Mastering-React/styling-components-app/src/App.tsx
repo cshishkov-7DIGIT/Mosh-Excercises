0fcd82125154707306f148de10514fa4f8105d47
cshishkov-7DIGIT <cshishkov@7digit.io>
2023-03-14 10:21:40 +0200

    update

---
+++ Mastering-React/styling-components-app/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import ListGroup from "./components/ListGroup";
+import { BsFillCalendarFill } from "react-icons/bs";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <ListGroup /> */}
+      <BsFillCalendarFill color="red" size="40" />
+    </div>
+  );
+}
+
+export default App;
---
+++ Mastering-React/styling-components-app/src/components/ListGroup/ListGroup.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styles from "./ListGroup.module.css";
+
+export default function ListGroup() {
+  let items = [
+    "New York",
+    "Los Angeles",
+    "Chicago",
+    "Houston",
+    "Philadelphia",
+    "Phoenix",
+    "San Antonio",
+  ];
+
+  let heading = "Cities";
+
+  return (
+    <div>
+      <h1>{heading}</h1>
+      <ul className={[styles.listGroup, styles.container].join(" ")}>
+        {items.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
